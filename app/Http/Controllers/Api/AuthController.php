7ec48221ad618fc4ee5cfee815129eaf192be8dc
Vinícius Oliveira <vinicius.oliveira@engeselt.com.br>
2026-04-23 23:39:34 -0300

    authenticate using sanctum

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Actions\Fortify\CreateNewUser;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register(Request $request, CreateNewUser $creator)
+    {
+        $user = $creator->create($request->all());
+
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('auth_token')->plainTextToken,
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::query()->where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['As credenciais informadas estão incorretas'],
+            ]);
+        }
+
+        $user->tokens()->delete();
+
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('auth_token')->plainTextToken,
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Logout realizado com sucesso!',
+        ]);
+    }
+}
